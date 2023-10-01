540ca03976985d7857cb29877d1747e6c624b065
deaneturner <deaneturner.com@gmail.com>
2023-09-30 22:47:32 -0400

    move demo to base app

---
+++ projects/bitnodepay-ui/src/app/components/auth/lockscreen/lockscreen.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {LayoutService} from "../../../layout/service/app.layout.service";
+// import { LayoutService } from 'src/app/layout/service/app.layout.service';
+
+@Component({
+    templateUrl: './lockscreen.component.html'
+})
+export class LockScreenComponent {
+
+    constructor(public layoutService: LayoutService) {}
+
+	get dark(): boolean {
+		return this.layoutService.config.colorScheme !== 'light';
+	}
+
+}
---
+++ projects/bitnodepay-ui/src/app/components/auth/newpassword/newpassword.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {LayoutService} from "../../../layout/service/app.layout.service";
+// import { LayoutService } from 'src/app/layout/service/app.layout.service';
+
+@Component({
+	templateUrl: './newpassword.component.html'
+})
+export class NewPasswordComponent {
+
+	rememberMe: boolean = false;
+
+	constructor(public layoutService: LayoutService) {}
+
+	get dark(): boolean {
+		return this.layoutService.config.colorScheme !== 'light';
+	}
+
+}
