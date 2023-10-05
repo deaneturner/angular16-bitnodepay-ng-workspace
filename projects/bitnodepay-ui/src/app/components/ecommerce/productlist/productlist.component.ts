aef897efbc5055b4ff0c0542369f3fbff6544df5
deaneturner <deaneturner.com@gmail.com>
2023-10-05 19:07:11 -0400

    connection management

--- projects/bitnodepay-ui/src/app/components/ecommerce/productlist/productlist.component.ts
+++ projects/bitnodepay-ui/src/app/components/ecommerce/productlist/productlist.component.ts
@@ -10,22 +10,27 @@ export class ProductListComponent {
   network = [
     {
       title: 'Starting and stopping a node',
+      description: 'Learn how to start and stop a node',
       type: 'Bitcoind Node Basics',
     },
     {
       title: 'LND Node Configuration',
+      description: 'Learn how to configure your LND node',
       type: 'Lightning Transfer',
     },
     {
       title: 'LND Node Configuration',
+      description: 'Learn how to configure your LND node',
       type: 'Ligtning Channel Creation',
     },
     {
       title: 'LND Node Configuration',
+      description: 'Learn how to configure your LND node',
       type: 'Lightning Channel Closure',
     },
     {
       title: 'LND Node Configuration',
+      description: 'Learn how to configure your LND node',
       type: 'Lightning Channel Balancing',
     },
     {
