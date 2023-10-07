335ff52fa0ee8a711590d047482dd2e28f4adf69
deaneturner <deaneturner.com@gmail.com>
2023-10-06 20:54:59 -0400

    xterm terminal and logs

--- projects/bitnodepay-ui/src/app/app.module.ts
+++ projects/bitnodepay-ui/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AppLayoutModule} from './layout/app.layout.module';
-import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
+import {SocketIoModule} from "ngx-socket-io";
 import {MessageModule} from "primeng/message";
 import {MessageService} from "primeng/api";
 import {config} from "./service/docker.container.service";
