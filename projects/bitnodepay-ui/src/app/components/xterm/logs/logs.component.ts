3cb9a1ba643e1ac3b6d349470bd5691b1aa6ae17
deaneturner <deaneturner.com@gmail.com>
2023-10-08 16:45:05 -0400

    xterm

--- projects/bitnodepay-ui/src/app/components/xterm/logs/logs.component.ts
+++ projects/bitnodepay-ui/src/app/components/xterm/logs/logs.component.ts
@@ -20,43 +20,46 @@ export class LogsComponent implements AfterViewInit {
   constructor(@Inject(DOCUMENT) private document: Document, private dockerContainerService: DockerContainerService) {
     const self = this;
     this.socket = this.dockerContainerService.socket;
-      this.terminal = new Terminal({
-        useStyle: true,
-        convertEol: true,
-        screenKeys: false,
-        cursorBlink: false,
-        visualBell: false,
-        colors: (Terminal as any).xtermColors // TODO - troubleshoot if setting: as any - is detrimental
-      } as any);  // TODO - troubleshoot if setting: as any - is detrimental
-      const fitAddon = new FitAddon();
-      this.terminal.loadAddon(fitAddon);
+    this.terminal = new Terminal({
+      useStyle: true,
+      convertEol: true,
+      screenKeys: false,
+      cursorBlink: false,
+      visualBell: false,
+      colors: (Terminal as any).xtermColors // TODO - troubleshoot if setting: as any - is detrimental
+    } as any);  // TODO - troubleshoot if setting: as any - is detrimental
+    const fitAddon = new FitAddon();
+    this.terminal.loadAddon(fitAddon);
 
-      window.onload = function () {
-        fitAddon.fit();
-      };
+    window.onload = function () {
+      fitAddon.fit();
+    };
 
-      this.terminal.onResize(function (event) {
-        const rows = event.rows;
-        const cols = event.cols;
-        console.log('resizing to', {cols: cols, rows: rows + 1});
-        self.socket.emit('resize', {cols: cols, rows: rows + 1});
-      });
+    this.terminal.onResize(function (event) {
+      const rows = event.rows;
+      const cols = event.cols;
+      console.log('resizing to', {cols: cols, rows: rows + 1});
+      self.socket.emit('resize', {cols: cols, rows: rows + 1});
+    });
 
-      // var id = window.location.pathname.split('/')[3];
-      // var host = window.location.origin;
-      // var socket = io.connect(host);
+    // var id = window.location.pathname.split('/')[3];
+    // var host = window.location.origin;
+    // var socket = io.connect(host);
 
-      this.socket.on('show', (data: any) => {
-        self.terminal.write(data);
-      });
+    this.socket.on('show', (data: any) => {
+      self.terminal.write(data);
+    });
 
-      this.socket.on('end', (status: any) => {
-        self.socket.disconnect();
-      });
-    }
+    this.socket.on('end', (status: any) => {
+      self.socket.disconnect();
+    });
+  }
 
   ngAfterViewInit(): void {
-    this.socket.emit('attach', 'terminal', this.terminalEl.nativeElement.width(), this.terminalEl.nativeElement.height());
+    // TODO: Fix
+    // this.socket.ioSocket.emit('attach', 'terminal', 300, 300);
+    // this.socket.emit('attach', 'terminal', this.terminalEl.nativeElement.width(), this.terminalEl.nativeElement.height());
+
     this.terminal.open(this.terminalEl.nativeElement);
-    }
+  }
 }
