5f6494f348dad87ab13cce62f01278a85c973e09
deaneturner <deaneturner.com@gmail.com>
2023-10-19 09:41:32 -0400

    environments

---
+++ projects/bitnodepay-ui/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://bitnodesim.com/api',
+  wsUrl: 'http://bitnodesim:3000',
+};
--- projects/bitnodepay-ui/src/environments/environment.ts
+++ projects/bitnodepay-ui/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://bitnodsim/api',
+  apiUrl: 'http://localhost:3000/api',
+  wsUrl: 'http://localhost:3000',
 };
