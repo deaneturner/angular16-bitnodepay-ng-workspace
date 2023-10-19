b4ca7974fac919f58ae70d744435b04ccfaac5aa
deaneturner <deaneturner.com@gmail.com>
2023-10-19 09:54:40 -0400

    prod build and test - localhost urls

--- projects/bitnodepay-ui/src/environments/environment.prod.ts
+++ projects/bitnodepay-ui/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bitnodesim.com/api',
-  wsUrl: 'http://bitnodesim:3000',
+  // apiUrl: 'http://bitnodesim.com/api',
+  // wsUrl: 'http://bitnodesim:3000',
+  apiUrl: 'http://localhost:3000/api',
+  wsUrl: 'http://localhost:3000',
 };
